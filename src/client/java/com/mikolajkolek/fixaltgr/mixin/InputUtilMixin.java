e1d75e7ccfd5b8996b8649b975491176da7c459f
MikolajKolek <kolek.mikolaj@gmail.com>
2024-06-25 13:58:35 +0200

    1.19.x Fabric backport

--- src/client/java/com/mikolajkolek/fixaltgr/mixin/InputUtilMixin.java
+++ src/client/java/com/mikolajkolek/fixaltgr/mixin/InputUtilMixin.java
@@ -1,7 +1,7 @@
 package com.mikolajkolek.fixaltgr.mixin;
 
-import com.mikolajkolek.fixaltgr.FixAltGr;
-import com.mojang.blaze3d.platform.InputUtil;
+import com.mikolajkolek.fixaltgr.FixAltGrClient;
+import net.minecraft.client.util.InputUtil;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -9,23 +9,23 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import java.util.concurrent.TimeUnit;
 
 @Mixin(InputUtil.class)
-public class InputUtilMixin {
+public class InputUtilMixin { //69696969696969 jubert to nooooobek
     @Inject(at = @At(value = "HEAD"), method = "isKeyPressed", cancellable = true)
-    private static void isKeyPressed(long handle, int code, CallbackInfoReturnable<Boolean> cir) {
+    private static void isKeyPressed(long window, int code, CallbackInfoReturnable<Boolean> cir) {
         if(code != 341) return;
 
-        if(!FixAltGr.listener.controlKeyPressed || FixAltGr.listener.altKeyPressed)
-            cir.setReturnValue(false);
+        if(!FixAltGrClient.listener.controlKeyPressed || FixAltGrClient.listener.altKeyPressed)
+			cir.setReturnValue(false);
         else {
             try {
                 TimeUnit.MILLISECONDS.sleep(10);
             }
 			catch (InterruptedException e) {
-                FixAltGr.LOGGER.error("The isKeyPressed sleep was interrupted!");
+				FixAltGrClient.LOGGER.error("The isKeyPressed sleep was interrupted!");
             }
 
-            if (!FixAltGr.listener.controlKeyPressed || FixAltGr.listener.altKeyPressed)
-                cir.setReturnValue(false);
+            if (!FixAltGrClient.listener.controlKeyPressed || FixAltGrClient.listener.altKeyPressed)
+				cir.setReturnValue(false);
             else
                 cir.setReturnValue(true);
         }
